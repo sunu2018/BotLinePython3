40622a7161724903949ea08651f676a43dac406a
sunu2018 <39335771+sunu2018@users.noreply.github.com>
2018-06-01 21:26:57 +0700

    ทดสอบ

--- coco4bot.py
+++ coco4bot.py
@@ -1,15 +1,4 @@
-# -*- coding: utf-8 -*-
 
-from linepy import *
-from datetime import datetime
-from time import sleep
-from bs4 import BeautifulSoup
-from humanfriendly import format_timespan, format_size, format_number, format_length
-import time, random, sys, json, codecs, threading, glob, re, string, os, requests, subprocess, six, ast, pytz, urllib, urllib.parse,timeit,data,atexit
-from gtts import gTTS
-from googletrans import Translator
-
-botStart = time.time()
 
 cl = LINE()
 cl.log("Auth Token : " + str(cl.authToken))
